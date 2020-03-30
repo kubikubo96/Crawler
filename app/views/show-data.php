9061dacf362e106a36917c595841d84f90008840
kubikubo <nguyentattien.1996@gmail.com>
2020-03-30 10:41:02 +0700

    edit table

--- app/views/show-data.php
+++ app/views/show-data.php
@@ -4,34 +4,43 @@
 <body>
 <div class="container">
     <h1 id="title-data">Dữ liệu đã thu thập được</h1>
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Title</th>
-            <th scope="col">Contents</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                ?>
-                <tr>
-                    <th scope="row"><?= $row['id'] ?></th>
-                    <td><?= $row['title'] ?></td>
-                    <td><?= $row['article'] ?></td>
-                </tr>
-                <?php
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Contents</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    ?>
+                    <tr>
+                        <th scope="row"><?= $row['id'] ?></th>
+                        <td>
+                            <div class="content-title">
+                                <?= $row['title'] ?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="content-article">
+                                <?= $row['article'] ?>
+                            </div>
+                        </td>
+                    </tr>
+                    <?php
+                }
+            } else {
+                echo "<br> Không có bản ghi nào trong CSDL";
             }
-        } else {
-            echo "<br> Không có bản ghi nào trong CSDL";
-        }
-        ?>
-        </tbody>
-    </table>
+            ?>
+            </tbody>
+        </table>
+    </div>
     <a href="index.php?controller=index" class="text-info"> &larr;Return</a>
-
 </div>
 </body>
 </html>
