e3766a726dac7f523f851531ecdc4b960c5d9c2b
kubikubo96 <kubikubo96@gmail.com>
2020-04-01 14:16:00 +0700

    format code

--- app/views/show-data.php
+++ app/views/show-data.php
@@ -3,49 +3,47 @@
 <?php include_once "app/views/head.php" ?>
 <body>
 <div class="container">
-    <div>
-        <h1 id="title-data">Data has been collected</h1>
-        <table class="table" id="table-crawler">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">DateTime</th>
-                <th scope="col">Title</th>
-                <th scope="col">Contents</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    ?>
-                    <tr>
-                        <th scope="row"><?= $row['id'] ?></th>
-                        <td>
-                            <div class="content-date-crawler">
-                                <?= $row['datetime'] ?>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="content-title-crawler">
-                                <?= $row['title'] ?>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="content-article-crawler">
-                                <?= $row['article'] ?>
-                            </div>
-                        </td>
-                    </tr>
-                    <?php
-                }
-            } else {
-                echo "<br> Không có bản ghi nào trong CSDL";
+    <h1 id="title-data">Data has been collected</h1>
+    <table class="table" id="table-crawler">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">DateTime</th>
+            <th scope="col">Title</th>
+            <th scope="col">Contents</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+                <tr>
+                    <th scope="row"><?= $row['id'] ?></th>
+                    <td>
+                        <div class="content-date-crawler">
+                            <?= $row['datetime'] ?>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="content-title-crawler">
+                            <?= $row['title'] ?>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="content-article-crawler">
+                            <?= $row['article'] ?>
+                        </div>
+                    </td>
+                </tr>
+                <?php
             }
-            ?>
-            </tbody>
-    </div>
-    <a href="index.php?controller=index" class="text-info"> &larr;Return</a>
+        } else {
+            echo "<br> Không có bản ghi nào trong CSDL";
+        }
+        ?>
+        </tbody>
+        <a href="index.php?controller=index" class="text-info"> &larr;Return</a>
 </div>
 </div>
 </body>
