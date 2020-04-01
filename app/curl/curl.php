b119ebf9ef6e22256b4db6475f738cd4cbbd1e86
kubikubo96 <kubikubo96@gmail.com>
2020-04-01 20:00:19 +0700

    create curl file

---
+++ app/curl/curl.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Curl;
+
+Class Curl
+{
+    public function getWebContents($uri)
+    {
+        // Initialize CURL
+        $curl = curl_init($uri);
+        // Set return
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $result = curl_exec($curl);
+        // Disconnect CURL, free
+        curl_close($curl);
+
+        return $result;
+    }
+}
