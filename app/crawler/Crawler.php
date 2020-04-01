bb98659345446f8af136e93731b3dc250742e701
kubikubo96 <kubikubo96@gmail.com>
2020-04-01 10:28:39 +0700

    change function getWebContent

--- app/crawler/Crawler.php
+++ app/crawler/Crawler.php
@@ -6,7 +6,15 @@ class Crawler
 {
     public function getWebContent($uri)
     {
-        return file_get_contents($uri);
+        $ch = curl_init($uri);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $result = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $result;
     }
 
     public function getSpecificData($regex, $uri)
