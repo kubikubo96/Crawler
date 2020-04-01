16b5fe97b84113659687964ac3c6d943b71dd1e0
kubikubo96 <kubikubo96@gmail.com>
2020-04-01 15:32:10 +0700

    delete function

--- app/crawler/Crawler.php
+++ app/crawler/Crawler.php
@@ -23,19 +23,4 @@ class Crawler implements CrawlerInterface
         preg_match_all($regex, $this->getWebContent($uri), $matches);
         return !empty($matches[1]) ? $matches[1] : FALSE;
     }
-
-    public function getTitle($regex, $uri)
-    {
-        return $this->getSpecificData($regex, $uri);
-    }
-
-    public function getArticle($regex, $uri)
-    {
-        return $this->getSpecificData($regex, $uri);
-    }
-
-    public function getDate($regex, $uri)
-    {
-        return $this->getSpecificData($regex, $uri);
-    }
 }
