db82588e469316e96cbe92bd0f0ed04bc51a8cbf
kubikubo96 <kubikubo96@gmail.com>
2020-04-03 16:59:19 +0700

    edit function getSpecificData

--- app/crawler/Crawler.php
+++ app/crawler/Crawler.php
@@ -6,9 +6,9 @@ use App\Curl\Curl;
 
 class Crawler extends Curl implements CrawlerInterface
 {
-    public function getSpecificData($regex, $uri)
+    public function getSpecificData($regex)
     {
-        preg_match_all($regex, $this->getWebContent($uri), $matches);
+        preg_match_all($regex, $this->getWebContent(), $matches);
         return !empty($matches[1]) ? $matches[1] : FALSE;
     }
 }
