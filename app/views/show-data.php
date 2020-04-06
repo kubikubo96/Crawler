0e4b618cbd6613ce7eb6d76075d4729a4972db9d
kubikubo <kubikubo96@gmail.com>
2020-04-06 18:15:10 +0700

    change html

--- app/views/show-data.php
+++ app/views/show-data.php
@@ -4,46 +4,49 @@
 <body>
 <div class="container">
     <h1 id="title-data">Data has been collected</h1>
-    <table class="table" id="table-crawler">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">DateTime</th>
-            <th scope="col">Title</th>
-            <th scope="col">Contents</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                ?>
-                <tr>
-                    <th scope="row"><?= $row['id'] ?></th>
-                    <td>
-                        <div class="content-date-crawler">
-                            <?= $row['datetime'] ?>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="content-title-crawler">
-                            <?= $row['title'] ?>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="content-article-crawler">
-                            <?= $row['article'] ?>
-                        </div>
-                    </td>
-                </tr>
-                <?php
+    <div class="table-responsive">
+        <table class="table" id="table-crawler">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">DateTime</th>
+                <th scope="col">Title</th>
+                <th scope="col">Contents</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    ?>
+                    <tr>
+                        <th scope="row"><?= $row['id'] ?></th>
+                        <td>
+                            <div class="content-date-crawler">
+                                <?= $row['datetime'] ?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="content-title-crawler">
+                                <?= $row['title'] ?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="content-article-crawler">
+                                <?= $row['article'] ?>
+                            </div>
+                        </td>
+                    </tr>
+                    <?php
+                }
+            } else {
+                echo "<br> Không có bản ghi nào trong CSDL";
             }
-        } else {
-            echo "<br> Không có bản ghi nào trong CSDL";
-        }
-        ?>
-        </tbody>
-        <a href="index.php?controller=index" class="text-info"> &larr;Return</a>
+            ?>
+            </tbody>
+            <a href="index.php?controller=index" class="text-info"> &larr;Return</a>
+
+    </div>
 </div>
 </div>
 </body>
