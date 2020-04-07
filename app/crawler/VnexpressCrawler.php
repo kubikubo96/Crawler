0d6ff8ea20000c2ee2f0e18b20842a5d2520f039
kubikubo <kubikubo96@gmail.com>
2020-04-07 15:09:23 +0700

    edit class VnexpressCrawler

--- app/crawler/VnexpressCrawler.php
+++ app/crawler/VnexpressCrawler.php
@@ -4,9 +4,9 @@ namespace App\Crawler;
 
 class VnexpressCrawler extends Crawler
 {
-    public $regexTitle = '/<h1 class="title_news_detail.*?">(.*?)<\/h1>/ms';
-    public $regexArticle = '/<article class="content_detail .*?>(.*?)<\/article>/ms';
-    public $regexDate = '/<span class="time.*?>(.*?)<\/span>/m';
+    public $regexTitle = '/<h1 class="title.*?">(.*?)<\/h1>/m';
+    public $regexArticle = '/<div class="sidebar-1".*?description">(.*?)<div class="sidebar-2">/ms';
+    public $regexDate = '/<span class="date">(.*?)<\/span>/ms';
 
 
 }
