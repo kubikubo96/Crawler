27a1ea7ba2afacfab5be25002c3d033834a49f55
kubikubo <kubikubo96@gmail.com>
2020-04-08 02:33:25 +0700

    create abstract_factory

--- app/abstract_factory/CrawlerInterface.php
+++ app/abstract_factory/CrawlerInterface.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Crawler;
+namespace App\Abstract_factory;
 
 interface CrawlerInterface
 {
     public function getTitle();
+
     public function getArticle();
+
     public function getDate();
 }
