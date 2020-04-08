aaf252a5ab133c3d82a2a7aa90c38c2fecaf4627
kubikubo <kubikubo96@gmail.com>
2020-04-08 13:28:56 +0700

    change storeDataController

--- app/controllers/StoreDataController.php
+++ app/controllers/StoreDataController.php
@@ -3,9 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\Model;
-use App\Abstract_factory\VnexpressFactory;
-use App\Abstract_factory\DantriFactory;
-use App\Abstract_factory\VietnamnetFactory;
+use App\Crawler\Crawler_factory\CrawlerFactory;
 use Exception;
 
 class StoreDataController
@@ -16,31 +14,23 @@ class StoreDataController
             $uri = $_POST['url'];
             $domain = substr($uri, 0, 22);
             $model = new Model();
-            if ($domain == "https://vnexpress.net/") {
-                $crawlerVnexpress = new VnexpressFactory($uri);
-                $title = $crawlerVnexpress->makeGetData()->getTitle()[0];
-                $article = $crawlerVnexpress->makeGetData()->getArticle()[0];
-                $datetime = $crawlerVnexpress->makeGetData()->getDate()[0];
-            } elseif ($domain == "https://dantri.com.vn/") {
-                $crawlerDantri = new DantriFactory($uri);
-                $title = $crawlerDantri->makeGetData()->getTitle()[0];
-                $article = $crawlerDantri->makeGetData()->getArticle()[0];
-                $datetime = $crawlerDantri->makeGetData()->getDate()[0];
-            } elseif ($domain == "https://vietnamnet.vn/") {
-                $crawlerVietnamnet = new VietnamnetFactory($uri);
-                $title = $crawlerVietnamnet->makeGetData()->getTitle()[0];
-                $article = $crawlerVietnamnet->makeGetData()->getArticle()[0];
-                $datetime = $crawlerVietnamnet->makeGetData()->getDate()[0];
-            } else {
-                echo "<h4>Retrieve only data from article details of dantri, vnexpress, vietnamnet<br>URL cannot be empty</h4>";
-                include_once "app/views/index.php";
-                return false;
-            }
             try {
-                $this->checkNull($title, $article, $datetime);
-                $status = $model->store($title, $article, $datetime);
+                $this->checkDomain($domain);
+                $crawlerFactory = new CrawlerFactory($uri);
+                $crawlerWeb = $crawlerFactory->getData($domain);
+
+                $title = $crawlerWeb->getTitle()[0];
+                $article = $crawlerWeb->getArticle()[0];
+                $datetime = $crawlerWeb->getDate()[0];
+
                 try {
-                    $this->checkStatus($status);
+                    $this->checkNull($title, $article, $datetime);
+                    $status = $model->store($title, $article, $datetime);
+                    try {
+                        $this->checkStatus($status);
+                    } catch (Exception $e) {
+                        echo $e->getMessage();
+                    }
                 } catch (Exception $e) {
                     echo $e->getMessage();
                 }
@@ -51,12 +41,11 @@ class StoreDataController
         include_once "app/views/index.php";
     }
 
-    public function checkStatus($status)
+    public function checkDomain($domain)
     {
-        if (!$status) {
-            throw new Exception("<h4>Data saving failed!</h4>");
+        if ($domain == "" || ($domain != "https://vnexpress.net/" && $domain != "https://dantri.com.vn/" && $domain != "https://vietnamnet.vn/")) {
+            throw new Exception("<h4>Retrieve only data from article details of dantri, vnexpress, vietnamnet<br>URL cannot be empty</h4>");
         }
-        echo "<h4>Data crawled successfully!</h4>";
         return true;
     }
 
@@ -68,4 +57,12 @@ class StoreDataController
         return true;
     }
 
+    public function checkStatus($status)
+    {
+        if (!$status) {
+            throw new Exception("<h4>Data saving failed!</h4>");
+        }
+        echo "<h4>Data crawled successfully!</h4>";
+        return true;
+    }
 }
