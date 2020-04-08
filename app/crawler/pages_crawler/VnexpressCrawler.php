65d8f23a1bd834044eba317dc9d789fb12b86d55
kubikubo <kubikubo96@gmail.com>
2020-04-08 13:29:20 +0700

    change VnexpressCrawler

--- app/crawler/pages_crawler/VnexpressCrawler.php
+++ app/crawler/pages_crawler/VnexpressCrawler.php
@@ -7,6 +7,4 @@ class VnexpressCrawler extends Crawler implements CrawlerInterface
     public $regexTitle = '/<h1 class="title.*?">(.*?)<\/h1>/m';
     public $regexArticle = '/<div class="sidebar-1".*?description">(.*?)<div class="sidebar-2">/ms';
     public $regexDate = '/<span class="date">(.*?)<\/span>/ms';
-
-
 }
