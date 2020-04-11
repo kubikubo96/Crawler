942abd317708d12a21ea63c77d1978da474e2b0c
kubikubo <kubikubo96@gmail.com>
2020-04-11 18:23:45 +0700

    create DanTriTest

---
+++ tests/crawler/DantriTest.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__ . "/../../app/crawler/CrawlerInterface.php";
+require_once __DIR__ . "/../../app/crawler/Crawler.php";
+require_once __DIR__ . "/../../app/crawler/pages/DantriCrawler.php";
+
+use PHPUnit\Framework\TestCase;
+use App\Crawler\DantriCrawler;
+
+class DantriTest extends TestCase
+{
+    protected $dantri;
+    protected $uri = "https://dantri.com.vn/xa-hoi/dem-nay-go-lenh-phong-toa-benh-vien-bach-mai-20200411161700001.htm";
+
+    public function setUp(): void
+    {
+        $this->dantri = new DantriCrawler($this->uri);
+    }
+
+    public function testTitleDantriReturnNotNull()
+    {
+        $this->assertNotEquals('', $this->dantri->getTitle());
+    }
+
+    public function testArticleDantriReturnNotNull()
+    {
+        $this->assertNotEquals('', $this->dantri->getArticle());
+    }
+
+    public function testDateDantriReturnNotNull()
+    {
+        $this->assertNotEquals('', $this->dantri->getDate());
+    }
+}
